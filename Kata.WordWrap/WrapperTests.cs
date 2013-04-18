3506ca9abe0425f7c12adcd9c7352c1a5ba66361
Gregor Woiwode <gregor.woiwode@gmail.com>
2013-04-18 20:58:50 +0200

    Added Kata Word Wrap.

---
+++ Kata.WordWrap/WrapperTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Kata.WordWrap
+{
+  // You write a class called Wrapper, that has a single
+  // static function named wrap that takes two arguments, 
+  // a string, and a column number. The function returns the string, 
+  // but with line breaks inserted at just the right places to make sure
+  // that no line is longer than the column number. You try to break 
+  // lines at word boundaries.
+  // Like a word processor, break the line by replacing the last space in a line with a newline.
+
+  [TestFixture]
+  public class WrapperTests
+  {
+    [TestCase("Hi my name is...", 20)]
+    public void When_the_wrapper_takes_a_text_shorter_than_the_collumn_number_it_should_return_the_text(string text, int columnNumber)
+    {
+      string actual = Wrapper.Wrap(text, columnNumber);
+
+      Assert.AreEqual(text, actual);
+    }
+
+    [TestCase("Hi my name is Gregor Jan.", 20)]
+    public void When_the_wrapper_takes_a_text_longer_than_the_collumn_number_it_should_break(string text, int columnNumber)
+    {
+      string expected = "Hi my name is \nGregor Jan.";
+
+      string actual = Wrapper.Wrap(text, columnNumber);
+
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestCase("Only insert a line break on word bounderies!", 20)]
+    public void When_a_line_reached_the_collumn_number_a_line_break_should_be_inserted_before(string text, int columnNumber)
+    {
+      string expected = "Only insert a line \nbreak on word \nbounderies!";
+
+      string actual = Wrapper.Wrap(text, columnNumber);
+
+      Assert.AreEqual(expected, actual);
+    }
+  }
+
+  public class Wrapper
+  {
+    public static string Wrap(string text, int lengthOfLine)
+    {
+      if (text.Length <= lengthOfLine)
+        return text;
+
+      var line = text.Substring(0, lengthOfLine - 1);
+      var rest = text.Substring(lengthOfLine - 1);
+      
+      var result = BreakLineBehindLastSpace(line);
+    
+      if (rest.Length >= lengthOfLine)
+        result += Wrap(rest, lengthOfLine);
+      else
+        result += rest;
+
+      return result;
+    }
+
+    public static string BreakLineBehindLastSpace(string line)
+    {
+      return line.Insert(BehindLastSpaceOf(line),
+                         "\n");
+    }
+
+    private static int BehindLastSpaceOf(string line)
+    {
+      return line.LastIndexOf(" ", StringComparison.Ordinal) + 1;
+    }
+  }
+}
