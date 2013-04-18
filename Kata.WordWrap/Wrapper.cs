07390d3d4e10f521d22c74701bda40e1d8966583
Gregor Woiwode <gregor.woiwode@gmail.com>
2013-04-18 21:03:32 +0200

    Moved Wrapper to own file.

---
+++ Kata.WordWrap/Wrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Kata.WordWrap
+{
+  public class Wrapper
+  {
+    public static string Wrap(string text, int lengthOfLine)
+    {
+      if (text.Length <= lengthOfLine)
+        return text;
+
+      var line = text.Substring(0, lengthOfLine - 1);
+      var rest = text.Substring(lengthOfLine - 1);
+      
+      var result = BreakLineBehindLastSpace(line);
+    
+      if (rest.Length >= lengthOfLine)
+        result += Wrap(rest, lengthOfLine);
+      else
+        result += rest;
+
+      return result;
+    }
+
+    public static string BreakLineBehindLastSpace(string line)
+    {
+      return line.Insert(BehindLastSpaceOf(line),
+                         "\n");
+    }
+
+    private static int BehindLastSpaceOf(string line)
+    {
+      return line.LastIndexOf(" ", StringComparison.Ordinal) + 1;
+    }
+  }
+}
--- Kata.WordWrap/WrapperTests.cs
+++ Kata.WordWrap/WrapperTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using NUnit.Framework;
 
 namespace Kata.WordWrap
@@ -41,38 +40,6 @@ namespace Kata.WordWrap
       string actual = Wrapper.Wrap(text, columnNumber);
 
       Assert.AreEqual(expected, actual);
-    }
-  }
-
-  public class Wrapper
-  {
-    public static string Wrap(string text, int lengthOfLine)
-    {
-      if (text.Length <= lengthOfLine)
-        return text;
-
-      var line = text.Substring(0, lengthOfLine - 1);
-      var rest = text.Substring(lengthOfLine - 1);
-      
-      var result = BreakLineBehindLastSpace(line);
-    
-      if (rest.Length >= lengthOfLine)
-        result += Wrap(rest, lengthOfLine);
-      else
-        result += rest;
-
-      return result;
-    }
-
-    public static string BreakLineBehindLastSpace(string line)
-    {
-      return line.Insert(BehindLastSpaceOf(line),
-                         "\n");
-    }
-
-    private static int BehindLastSpaceOf(string line)
-    {
-      return line.LastIndexOf(" ", StringComparison.Ordinal) + 1;
     }
   }
 }
